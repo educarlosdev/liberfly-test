82cf2036de5a364749f66aab4751e43cd05e45ca
Eduardo Carlos <duureis@gmail.com>
2023-05-19 20:10:31 -0300

    instalação pacote swagger configuração inicial documentação Auth

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Liberfly Documetantion Test",
+ *      description="L5 Swagger OpenApi description",
+ *      @OA\Contact(
+ *          email="duureis@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Demo API Server"
+ * )
+
+ *
+ * @OA\Tag(
+ *     name="Auth",
+ *     description="API Endpoints de Autenticação"
+ * )
+ */
 
 class Controller extends BaseController
 {
